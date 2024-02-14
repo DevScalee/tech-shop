0c8cf7f4270c799c956d0f3e1318e64767215e41
Ghofranbtk <ghofran.bentk@gmail.com>
2024-02-14 22:29:55 +0100

    add components in frontend

---
+++ tech-shop-web/src/app/components/private/client/view-product-details/view-product-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-product-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-product-details.component.html',
+  styleUrl: './view-product-details.component.scss'
+})
+export class ViewProductDetailsComponent {
+
+}
