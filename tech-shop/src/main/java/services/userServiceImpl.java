c13becd7278951b97842bae48e4ca9cff98457ac
Aziz-Guesmi <aguesmi44@gmail.com>
2024-02-17 16:12:19 +0100

    user back end + setting up structure

---
+++ tech-shop/src/main/java/services/userServiceImpl.java
@@ -0,0 +1,13 @@
+package services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.idl.techshop.repository.UserRepository;
+
+@Service
+public class userServiceImpl implements userService {
+
+	@Autowired
+	UserRepository userRepo;
+}
