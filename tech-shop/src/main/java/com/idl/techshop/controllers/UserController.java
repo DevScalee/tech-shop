df14d7af7b5144a2482e019fd789bd5270e32894
Ghofranbtk <ghofran.bentk@gmail.com>
2024-02-17 18:48:24 +0100

    add backend configuration

---
+++ tech-shop/src/main/java/com/idl/techshop/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.idl.techshop.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.idl.techshop.services.userService;
+
+@CrossOrigin(origins="*", maxAge=3600)
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+	
+	@Autowired
+	userService userS;
+
+}
---
+++ tech-shop/src/main/java/com/idl/techshop/models/User.java
@@ -0,0 +1,68 @@
+package com.idl.techshop.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="userid")
+	private Long id ;
+	
+	@NotBlank
+	@Size(max = 25)
+	private String name ;
+	
+	@NotBlank
+	@Size(max = 25)
+	private String lastName ;
+	
+	@Email 
+	private String email ;
+	
+	 @NotBlank
+	 @Size(max = 120)
+	 private String password;
+	  
+	  
+	  private Boolean accountStatus ;
+
+	  @Column(name="resetToken")
+	  private String resetToken;
+	  
+
+	  
+	@Column(name="confirmToken")
+	private String confirmToken;
+	
+	
+	@Enumerated(EnumType.STRING)
+	  private Role role ;
+
+
+	
+	
+	
+	
+
+	
+}
