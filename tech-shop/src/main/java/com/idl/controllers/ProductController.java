210d07e490d07f6fa9dec537a5092f15ab2b8d59
Aziz-Guesmi <aguesmi44@gmail.com>
2024-02-18 16:20:45 +0100

    removing duplicated files

--- tech-shop/src/main/java/com/idl/controllers/ProductController.java
+++ tech-shop/src/main/java/com/idl/controllers/ProductController.java
@@ -1,5 +1,20 @@
 package com.idl.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.idl.services.ProductService;
+import com.idl.models.Product;
+import jakarta.validation.constraints.AssertFalse.List;
+
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
+	
+	@Autowired
+	ProductService productS;
+	
 
 }
