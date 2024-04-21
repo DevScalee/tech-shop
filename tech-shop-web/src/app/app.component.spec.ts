1c504a755dc951d422d7e816ae8861199fb3a2c6
Ghofranbtk <ghofran.bentk@gmail.com>
2024-04-21 11:58:51 +0200

    add front

--- tech-shop-web/src/app/app.component.spec.ts
+++ tech-shop-web/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tech-shop-web' title`, () => {
+  it(`should have as title 'Projet'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tech-shop-web');
+    expect(app.title).toEqual('Projet');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tech-shop-web');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Projet app is running!');
   });
 });
