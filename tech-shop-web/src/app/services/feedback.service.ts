9ac7b2fdd780ff107e1d7ebee7ae1d1ca24c3cd8
Ghofran Ben Tkhayat <75264726+Ghofranbtk@users.noreply.github.com>
2024-05-12 21:58:46 +0200

    finale

--- tech-shop-web/src/app/services/feedback.service.ts
+++ tech-shop-web/src/app/services/feedback.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class FeedbackService {
-  feedbackUrl: string = 'http://localhost:3001';
+  feedbackUrl: string = 'http://localhost:9050';
 
   constructor(private httpClient: HttpClient) {}
 
